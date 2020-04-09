ede9ab7baa0977d7cf57d5cff6762ac14dec3fad
sitnikov <oleg.sitnikov@icloud.com>
2020-04-09 09:55:59 +0300

    Rename package

--- app/src/androidTest/java/com/zovjoin/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/zovjoin/myapplication/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.zovjoin.myapplication;
 
 import android.content.Context;
 
--- app/src/main/java/com/zovjoin/myapplication/MainActivity.java
+++ app/src/main/java/com/zovjoin/myapplication/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.zovjoin.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
