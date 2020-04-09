c5576136bed7a970aeca8f37f56474c2ba65bfbf
sitnikov <oleg.sitnikov@icloud.com>
2020-04-09 09:47:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView mywebview = findViewById(R.id.mywebview);
+        mywebview.getSettings().setJavaScriptEnabled(true);
+        mywebview.loadUrl("https://t.zovjoin.com/");
+        mywebview.setWebViewClient(new WebViewClient());
+    }
+}
